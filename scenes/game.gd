8aea20fc4bc2975a2b2c2085281377ac0e6ae8d2
illogicalapple <80916557+illogicalapple@users.noreply.github.com>
2025-06-24 18:43:03 -0500

    shit

--- scenes/game.gd
+++ scenes/game.gd
@@ -1,6 +1,6 @@
 extends Node
 
 func _ready():
-	Inventory.set_slot(0, Inventory.Card.new("punch"))
-	Inventory.set_slot(1, Inventory.Card.new("punch"))
+	Inventory.set_slot(0, Inventory.Card.new("red potion"))
+	Inventory.set_slot(1, Inventory.Card.new("nuke"))
 	Inventory.set_slot(2, Inventory.Card.new("punch"))
